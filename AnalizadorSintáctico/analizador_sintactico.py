c03d563ab5ead9c0769355b10df605eb823b0d64
Fabricio Becerra Quezada <164562091+FabrixBq@users.noreply.github.com>
2025-05-25 16:54:12 -0600

    Update analizador_sintactico.py

--- AnalizadorSintáctico/analizador_sintactico.py
+++ AnalizadorSintáctico/analizador_sintactico.py
@@ -1,10 +1,12 @@
 import AnalizadorLexico as al
 import GeneradorEstructurasG as gen
 import Pila as p
+import AnalizadorLexico as lexico
 
 class AnalizadorSintactico:
-    def __init__(self, analizador_lexico):
-        self.analizador_lexico = analizador_lexico
+    def __init__(self):
+        # Inicializa el analizador léxico
+        self.analizador_lexico = lexico.AnalizadorLexico()
         self.pila = p.Pila()
         self.token_actual = None
         self.estructuras = gen.GeneradorEstructurasG()
@@ -14,51 +16,66 @@ class AnalizadorSintactico:
         self.terminales = self.estructuras.getTerminales()
         self.matrizPredictiva = self.estructuras.getMatrizPredictiva()
 
+    def imprimir_tokens(self):
+        while True:
+            token = self.analizador_lexico.scanner()  # Obtener el siguiente token del analizador léxico
+            if token is None:
+                break
+            print(token)
+
     def LlDriver(self):
         self.pila.push(self.noterminales[0])  # Agregar el símbolo inicial a la pila
         x = self.pila.peek()  # Obtener el símbolo en la parte superior de la pila
         a = self.analizador_lexico.scanner()  # Obtener el token de entrada del analizador léxico
         while self.pila.isEmpty() == False:
             if x in self.noterminales:
-                if self.matrizPredictiva[x,a] != 0:
-                    x = self.derivaciones[self.matrizPredictiva[x,a]-1]
+                if self.matrizPredictiva[self.noterminales.index(x)][self.terminales.index(a)] != 0:
+                    x = self.derivaciones[self.matrizPredictiva[self.noterminales.index(x)][self.terminales.index(a)]-1]
                     self.pila.pop()
                     self.cicloPush(x)
+                    a = self.analizador_lexico.scanner()
                 else:
-                    self.procesarErrorSintactico()
+                    self.procesarErrorSintactico(a)
+                    a = self.analizador_lexico.scanner()
             else:
                 if x == a:
                     self.pila.pop()
                     a = self.analizador_lexico.scanner()
                 else:
-                    self.procesarErrorSintactico()
+                    self.procesarErrorSintactico(a)
+                    a = self.analizador_lexico.scanner()
 
     # Método para meter los simbolos de las derivaciones a la pila
     def cicloPush(self, x):
         cursorDer = len(x)-1
         simbolo = ""
         while cursorDer >= 0:
-            if x[cursorDer] != " ":
-                simbolo += x[cursorDer]
-            else:
-                self.pila.push(simbolo[cursorDer+1:len(simbolo)])
+            if x[cursorDer] == " ":
+                self.pila.push(simbolo[::-1])  # Invertir el símbolo antes de agregarlo a la pila
                 simbolo = ""
+            else:
+                simbolo += x[cursorDer]
             cursorDer -= 1
+        print("Pila: ", self.pila)
 
-    def procesarErrorSintactico(self):
+    def procesarErrorSintactico(self, token):
         print(f"Error sintáctico: token inesperado '{token}'")
-        # Agregar Lógica de manejo de errores
+        self.pila.pop()  # Ignorar el token inesperado
 
-class AnaliadorSintactico:
-    def __init__(self, lexico):
-        self.lexico = lexico
-    
-    def imprimir_tokens(self):
-        while True:
-            token = self.lexico.scanner()
-            if token is None:
-                break
-            print(token)
+    def main(self):
+        # Imprime la gramática, derivaciones, no terminales y terminales
+        self.estructuras.printGramatica()
+        self.estructuras.printDerivacion()
+        self.estructuras.printNoTerminales()
+        self.estructuras.printTerminales()
+        self.estructuras.printMatrizPredictiva()
+        # Inicia el análisis sintáctico
+        self.LlDriver()
 
-    
-    
+if __name__ == "__main__":
+    # Crea una instancia del analizador léxico
+    analizador_lexico = al.AnalizadorLexico()
+    # Crea una instancia del analizador sintáctico 
+    analizador_sintactico = AnalizadorSintactico()
+    # Llama al método principal del analizador sintáctico
+    analizador_sintactico.main()

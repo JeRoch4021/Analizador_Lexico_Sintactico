8246263b535d799d5cce5b241c754ac804d497b7
Fabricio Becerra Quezada <164562091+FabrixBq@users.noreply.github.com>
2025-05-25 21:29:27 -0600

    Update analizador_sintactico.py

--- AnalizadorSintáctico/analizador_sintactico.py
+++ AnalizadorSintáctico/analizador_sintactico.py
@@ -25,42 +25,68 @@ class AnalizadorSintactico:
 
     def LlDriver(self):
         self.pila.push(self.noterminales[0])  # Agregar el símbolo inicial a la pila
-        x = self.pila.peek()  # Obtener el símbolo en la parte superior de la pila
+        #x = self.pila.peek() # Actualiza x con el símbolo en la parte superior de la pila
         a = self.analizador_lexico.scanner()  # Obtener el token de entrada del analizador léxico
         while self.pila.isEmpty() == False:
+            x = self.pila.peek()  # Actualiza x con el símbolo en la parte superior de la pila
             if x in self.noterminales:
                 if self.matrizPredictiva[self.noterminales.index(x)][self.terminales.index(a)] != 0:
                     x = self.derivaciones[self.matrizPredictiva[self.noterminales.index(x)][self.terminales.index(a)]-1]
                     self.pila.pop()
-                    self.cicloPush(x)
-                    a = self.analizador_lexico.scanner()
+                    self.cicloPush(self.stripCadena(x)) # Agrega los símbolos de la derivación a la pila
                 else:
                     self.procesarErrorSintactico(a)
-                    a = self.analizador_lexico.scanner()
             else:
-                if x == a:
+                if self.coinciden(x, a) or x == a:
                     self.pila.pop()
                     a = self.analizador_lexico.scanner()
                 else:
                     self.procesarErrorSintactico(a)
-                    a = self.analizador_lexico.scanner()
+            print("Pila actual: ", self.pila)
 
     # Método para meter los simbolos de las derivaciones a la pila
     def cicloPush(self, x):
-        cursorDer = len(x)-1
-        simbolo = ""
-        while cursorDer >= 0:
-            if x[cursorDer] == " ":
-                self.pila.push(simbolo[::-1])  # Invertir el símbolo antes de agregarlo a la pila
-                simbolo = ""
-            else:
-                simbolo += x[cursorDer]
-            cursorDer -= 1
-        print("Pila: ", self.pila)
+        print("Derivación: ", x)
+        cursor1 = len(x)-1
+        cursor2 = len(x)-1
+        while cursor1 >= 0:
+            if x[cursor1-1] == " " or cursor1 == 0:  # Verifica si es un espacio o el inicio de la cadena
+                print("metiendo a la pila: ", x[cursor1:cursor2])
+                self.pila.push(self.stripCadena(x[cursor1:cursor2]))  # Agrega el símbolo a la pila
+                cursor2 = cursor1  # Actualiza el cursor2 al inicio del siguiente símbolo
+            cursor1 -= 1
+    
+    def coinciden(self, x, a):
+        # Verifica si la parte superior de la pila (x) coincide con el token actual (a) en cuanto a clasificación lexica
+        clasificacionA = self.analizador_lexico.clasificar_token(a)
+        print(a," Clasificación del token actual: ", clasificacionA," | Símbolo en la pila - {x}: ", x)
+        if clasificacionA == 'Identificador' and x == 'id':
+            return True
+        elif clasificacionA == 'Numero Binario' and x == 'litbinaria':
+            return True
+        elif clasificacionA == 'Numero Octal' and x == 'litoctal':
+            return True
+        elif clasificacionA == 'Numero Hexadecimal' and x == 'lithexadecimal':
+            return True
+        else:
+            return False
 
     def procesarErrorSintactico(self, token):
         print(f"Error sintáctico: token inesperado '{token}'")
         self.pila.pop()  # Ignorar el token inesperado
+    
+    def stripCadena(self, cadena: str) -> str:
+        caracteres_no_validos = " \n\t\r"
+        inicio = 0
+        fin = len(cadena) - 1
+
+        while inicio <= fin and cadena[inicio] in caracteres_no_validos:
+            inicio += 1
+
+        while fin >= inicio and cadena[fin] in caracteres_no_validos:
+            fin -= 1
+        
+        return cadena[inicio:fin + 1] # Devuelve una subcadena desde el inicio hasta el fin + 1 
 
     def main(self):
         # Imprime la gramática, derivaciones, no terminales y terminales

4d2582a359cf79cfb32c8bb4ed1be546797664d3
Fabricio Becerra Quezada <164562091+FabrixBq@users.noreply.github.com>
2025-06-05 10:12:41 -0600

    Update AFN_Correccion.py

--- AnalizadorLexico/AFN_Correccion.py
+++ AnalizadorLexico/AFN_Correccion.py
@@ -11,55 +11,83 @@ class automata:
             'escribir': 262, 
             'finprograma': 263
         }
-        # Identificadores: Token 270
-        # Números Binarios: Token 271
-        # Números Octales: Token 272
-        # Números Hexadecimales: Token 273
+        # Identificadores: Token 261
+        # Números Binarios: Token 520
+        # Números Octales: Token 732
+        # Números Hexadecimales: Token 891
         # Caracteres Simples: Token ASCII  
     
-    def transiciones(self, estado, simbolo: str) -> str | None:
+    def transiciones(self, estado: int, simbolo: str) -> str | None:
         inicio = 0
         fin = len(simbolo) - 1
         match estado:
             case 0:
-                if simbolo[inicio] in '01' and simbolo[fin] == 'B':
+                if simbolo[inicio] in self.atributos_reservados:
+                    estado = 6
+                elif simbolo[inicio] in '01' and simbolo[fin] == 'B':
                     estado = 1
+                elif simbolo[inicio] in '01234567' and simbolo[fin] == 'O':
+                    estado = 2
+                elif simbolo[inicio] in '0123456789ABCDEF' and simbolo[fin] == 'X':
+                    estado = 3
+                elif simbolo[inicio] in 'abcdefghijklmnopqrstuvwxyz':
+                    estado = 4
+                elif simbolo[inicio] in '()*+,-/;=':
+                    estado = 5
+                else:
+                    estado = 7
                 return estado
             case 1:
-
-                return None
+                while inicio < fin:
+                    if simbolo[inicio] in '01':
+                        inicio += 1
+                    else:
+                        estado = 7 # Error Lexico
+                        return estado
+                return self.generarToken(520)  # Número Binario
             case 2:
-                return None
+                while inicio < fin:
+                    if simbolo[inicio] in '01234567':
+                        inicio += 1
+                    else:
+                        estado = 7  # Error Lexico
+                        return estado
+                return self.generarToken(732)  # Número Octal
             case 3:
-                return None
+                while inicio < fin:
+                    if simbolo[inicio] in '0123456789ABCDEF':
+                        inicio += 1
+                    else:
+                        estado = 7 # Error Lexico
+                        return estado
+                return self.generarToken(891)  # Número Hexadecimal
             case 4:
-                return None
+                while inicio < fin:
+                    if simbolo[inicio] in 'abcdefghijklmnopqrstuvwxyz':
+                        inicio += 1
+                    else:
+                        estado = 7  # Error Lexico
+                        return estado
+                return self.generarToken(261)  # Identificador
             case 5:
-                return None
+                return self.generarToken(ord(simbolo[inicio]))  # Carácter Simple
             case 6:
-                return None
+                return self.generarToken(self.atributos_reservados[simbolo[inicio]]) # Palabra Reservada
             case 7:
-                return None
-            case 8:
-                return None
-            case 9:
-                if simbolo[inicio] in '':
-                return None
-            case 10:
-                return 'Error Lexico'
+                return self.generarToken(0) # Error Lexico
             
     def generarToken(self, numero: int) -> str:
         if numero in self.atributos_reservados:
             return "Palabra Reservada"
-        elif numero == 270:
+        elif numero == 261:
             return "Identificador"
-        elif numero == 271:
+        elif numero == 520:
             return "Número Binario"
-        elif numero == 272:
+        elif numero == 732:
             return "Número Octal"
-        elif numero == 273:
+        elif numero == 891:
             return "Número Hexadecimal"
-        elif numero in ";=+-*()/,":
+        elif numero in range(39, 62):
             return "Carácter Simple"
         else:
             return "Error Lexico"

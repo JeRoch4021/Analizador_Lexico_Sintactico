078264c0fc789e0430d88b2f02b7d48d1cead332
JeRoch4021 <161788973+JeRoch4021@users.noreply.github.com>
2025-06-05 20:39:25 -0600

    Update pila.py

--- AnalizadorLexico/pila.py
+++ AnalizadorLexico/pila.py
@@ -5,6 +5,16 @@ class Pila:
         self.fin = None
         self.tamanio = 0
     
+    class Nodo:
+
+        def __init__(self, dato):
+            self.dato = dato
+            self.der = None
+            self.izq = None
+
+
+        def __str__(self):
+            return str(self.dato)
     
     def push(self, dato):
         # Crea un nuevo nodo con el dato a insertar
@@ -19,27 +29,6 @@ class Pila:
             self.inicio.izq = nuevo_nodo
             self.inicio = nuevo_nodo
         self.tamanio += 1
-
-
-    def pop(self):
-        # Nodo vacío temporal por si la pila está vacía
-        borrado = self.Nodo(None)
-        if self.inicio is not None:
-            if self.tamanio == 1:
-                # Si solo hay un nodo, se elimina y se reinicia la pila
-                borrado = self.inicio
-                self.inicio = None
-                self.fin = None
-                self.tamanio = 0
-            else:
-                # Elimina el nodo del tope (inicio)
-                borrado = self.inicio
-                self.inicio.izq = None
-                self.inicio = self.inicio.der
-                self.tamanio -= 1
-        # Devuelve el nodo completo eliminado (no solo el dato)
-        return borrado
-    
     
     def popDat(self):
         dato = None
@@ -57,34 +46,3 @@ class Pila:
             self.tamanio -= 1
         # Devuelve solo el dato del nodo, no el nodo completo
         return dato
-    
-    
-    def peek(self):
-        if self.inicio is not None:
-            # Devuelve el dato del nodo en el tope (sin quitarlo)
-            return self.inicio.dato
-        # Si la pila está vacía
-        return None
-    
-    
-    def imprimir(self):
-        salida = "Lista(" + str(self.size()) + "): {"
-        cursor = self.inicio
-        while cursor is not None:
-            salida += str(cursor.dato) + ", "
-            # Avanza al siguiente nodo
-            cursor = cursor.der
-        # Devuelve cadena que representa visualmente la pila
-        return salida + "\b}"
-    
-
-    class Nodo:
-
-        def __init__(self, dato):
-            self.dato = dato
-            self.der = None
-            self.izq = None
-
-
-        def __str__(self):
-            return str(self.dato)
